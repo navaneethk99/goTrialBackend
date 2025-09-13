ee26122886837e9f0ce59aea2d2891bcc80e8ed5
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-09-14 02:10:30 +0530

    added models

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-  "fmt"
+	"crudProject/database"
+	"github.com/gofiber/fiber/v2"
 )
+import "log"
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("API is running successfully")
+}
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
-
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	database.ConnectDb()
+	app := fiber.New()
+	app.Get("/", welcome)
+	log.Fatal(app.Listen(":8000"))
 }
