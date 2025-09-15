e896c6e6027d9b468b369e3319fadc2e2e549169
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-09-15 15:52:42 +0530

    added crud for users

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crudProject/database"
 	"crudProject/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 import "log"
 
@@ -19,11 +20,19 @@ func setupRoutes(app *fiber.App) {
 	app.Post("/users", routes.CreateUser)
 	app.Get("/users", routes.GetUsers)
 	app.Get("/users/:id", routes.GetUser)
+	app.Put("/users/:id", routes.UpdateUser)
+	app.Delete("/users/:id", routes.DeleteUser)
 }
 
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000", // frontend origin
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	setupRoutes(app)
 
--- routes/user.go
+++ routes/user.go
@@ -50,6 +50,7 @@ func FindUser(id int, user *models.User) error {
 	}
 	return nil
 }
+
 func GetUser(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	var user models.User
@@ -64,4 +65,54 @@ func GetUser(c *fiber.Ctx) error {
 
 	responseUser := CreateResponseUser(user)
 	return c.Status(200).JSON(responseUser)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := FindUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	type UpdateUser struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+	}
+
+	var updateData UpdateUser
+
+	if err := c.BodyParser(&updateData); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	user.FirstName = updateData.FirstName
+	user.LastName = updateData.LastName
+	database.Database.Db.Save(&user)
+
+	responseUser := CreateResponseUser(user)
+	return c.Status(200).JSON(responseUser)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := FindUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := database.Database.Db.Delete(&user).Error; err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.Status(200).JSON("User deleted")
 }
